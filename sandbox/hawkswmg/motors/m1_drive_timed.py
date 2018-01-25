d25f7264395ced750bb9fcee3b5bb775a6ffd9b4
levineha <levineha@rose-hulman.edu>
2018-01-25 15:07:49 -0500

    todo 3 woohoo

--- sandbox/hawkswmg/motors/m1_drive_timed.py
+++ sandbox/hawkswmg/motors/m1_drive_timed.py
@@ -64,7 +64,7 @@ import time
 #   Note: To repeat again, in later modules you will learn different (better) ways to travel a given distance using
 #     motor encoders, so just make a simple rough approximation here, since later we'll do it better in a different way.
 #
-# TODO: 3. Copy the content of the /examples/motors/drive_input_speed.py program and place it below these comments.
+# DONE: 3. Copy the content of the /examples/motors/drive_input_speed.py program and place it below these comments.
 #   Change the initial print and speak commands to reflect this module, like this...
 #    print("--------------------------------------------")
 #    print("  Timed Driving")
