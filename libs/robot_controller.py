84cd835830b6590535f8459946033f546ad8c0fb
hawkswmg <hawkswmg@rose-hulman.edu>
2018-01-31 14:44:54 -0500

    m3 done hawkswmg

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -19,9 +19,27 @@ import time
 class Snatch3r(object):
     """Commands for the Snatch3r robot that might be useful in many different programs."""
     
-    # TODO: Implement the Snatch3r class as needed when working the sandox exercises
+    # DONE: Implement the Snatch3r class as needed when working the sandox
+    # exercises
     # (and delete these comments)
     def __init__(self):
         """ construct and store a left motor and a right motor."""
-        self.left_motor = None
-        assert self.left_motor
+        self.left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+        self.right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
+        assert self.left_motor.connected
+        assert self.right_motor.connected
+
+    def drive_inches(self,inches_target, speed_deg_per_second):
+
+        time_s = 1
+        while time_s != 0:
+            all_sp = speed_deg_per_second
+            distance_s = inches_target
+            self.left_motor.run_to_rel_pos(position_sp=distance_s * 90,
+                                          speed_sp=
+            all_sp, stop_action='brake')
+            self.right_motor.run_to_rel_pos(position_sp=distance_s * 90,
+                                          speed_sp=
+            all_sp, stop_action='brake')
+            self.left_motor.wait_while(ev3.Motor.stop_action)
+            break
--- sandbox/hawkswmg/motors/m2_drive_encoders.py
+++ sandbox/hawkswmg/motors/m2_drive_encoders.py
@@ -10,7 +10,8 @@ Authors: David Fisher and Megan Hawksworth.
 import ev3dev.ev3 as ev3
 import time
 
-# TODO: 2. Copy the contents of your m1_drive_timed.py and paste that text into this file below these comments.
+# DONE: 2. Copy the contents of your m1_drive_timed.py and paste that text
+# into this file below these comments.
 #   If your program says and prints anything at the start change it to print and say "Drive using encoders"
 
 def main():
@@ -31,13 +32,11 @@ def main():
     while time_s != 0:
         all_sp = int(input("Enter a speed (0 to 900 dps): "))
         distance_s = int(input("Distance to travel (inches): "))
-        left_motor.run_to_rel_pos(position_sp = distance_s * 90, speed_sp=
-        all_sp, stop_action = 'brake')
+        left_motor.run_to_rel_pos(position_sp=distance_s * 90, speed_sp=
+        all_sp,stop_action='brake')
         right_motor.run_to_rel_pos(position_sp=distance_s * 90, speed_sp=
         all_sp, stop_action='brake')
-        left_motor.wait_while('state_running')
-        left_motor.stop()
-        right_motor.stop()
+        left_motor.wait_while(ev3.Motor.stop_action)
         ev3.Sound.beep().wait()
 
     print("Goodbye!")
@@ -50,7 +49,8 @@ main()
 # your code to hear the beep AFTER movement.
 #   ev3.Sound.beep().wait()
 
-# TODO: 4. Instead of using the run_forever, time.sleep, stop pattern switch to using the run_to_rel_pos command.
+# DONE: 4. Instead of using the run_forever, time.sleep, stop pattern switch
+# to using the run_to_rel_pos command.
 #   You will need to determine the position_sp value to pass into the run_to_rel_pos command as a named argument.
 #   Assume the diameter of the wheel is 1.3" (close enough).  A 1.3" diameter wheel results in approximately a 4"
 #     circumference, so 360 degrees = 4 inches of travel.
@@ -65,9 +65,11 @@ main()
 #        -- position_sp
 #        -- speed_sp
 #        -- stop_action
-# TODO: 5. Make sure the beep happens AFTER the motors stop.  Use the wait_while command to block code execution.
+# DONE: 5. Make sure the beep happens AFTER the motors stop.  Use the
+# wait_while command to block code execution.
 
-# TODO: 6. Formally test your work. When you think you have the problem complete run these tests:
+# DONE: 6. Formally test your work. When you think you have the problem
+# complete run these tests:
 #   200 dps 24 inches (make sure it drives within 2 inches of the target distance)
 #   400 dps 24 inches (make sure it drives within 2 inches of the target distance)
 #   800 dps 24 inches (make sure it drives within 2 inches of the target distance)
@@ -76,7 +78,8 @@ main()
 #   400 dps -36 inches (make sure it drives within 3 inches of the target distance)
 # Add more tests as you see fit.  Ideally you should be +/- 10% of the target goal this time.
 
-# TODO: 7. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.
+# DONE: 7. Call over a TA or instructor to sign your team's checkoff sheet
+# and do a code review.
 #
 # Observations you should make, run_to_rel_pos is easier to use since it uses encoders that are independent of speed.
 
