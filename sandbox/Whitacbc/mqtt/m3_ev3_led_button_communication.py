deb13d3cc84f0dca1c47f80f2eedfa90f3c41ac8
hawkswmg <hawkswmg@rose-hulman.edu>
2018-02-07 13:34:53 -0500

    m2 done hawkswmg

--- sandbox/Whitacbc/mqtt/m3_ev3_led_button_communication.py
+++ sandbox/Whitacbc/mqtt/m3_ev3_led_button_communication.py
@@ -32,8 +32,8 @@ communication.  Summary of the communication:
 Implement the TODOs below to complete this module, then transfer the file to the EV3 (as done in many previous units),
   then run this module on the EV3 while at the same time, running m3_pc_led_button_communication.py on your PC.
 
-Authors: David Fisher and Brian Whitacre.
-"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher and PUT_YOUR_NAME_HERE.
+"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 import mqtt_remote_method_calls as com
@@ -42,7 +42,7 @@ import ev3dev.ev3 as ev3
 import time
 
 
-# DONE: 2. Within the MyDelegate class below add the method, set_led, to receive messages as described above.
+# TODO: 2. Within the MyDelegate class below add the method, set_led, to receive messages as described above.
 # Here is some code that will likely be VERY useful in that method to convert the led_side_string and led_color_string
 #   into a useful led_side and led_color values that can be used with the ev3.Leds.set_color method.
 #
@@ -72,28 +72,6 @@ class MyDelegate(object):
     def __init__(self):
         self.running = True
 
-    def set_led(self,led_side_string,led_color_string):
-        print("Received: {} {}".format(led_side_string, led_color_string))
-        led_side = None
-        if led_side_string == "left":
-            led_side = ev3.Leds.LEFT
-        elif led_side_string == "right":
-            led_side = ev3.Leds.RIGHT
-
-        led_color = None
-        if led_color_string == "green":
-            led_color = ev3.Leds.GREEN
-        elif led_color_string == "red":
-            led_color = ev3.Leds.RED
-        elif led_color_string == "black":
-            led_color = ev3.Leds.BLACK
-
-        if led_side is None or led_color is None:
-            print("Invalid parameters sent to set_led. led_side_string = {} led_color_string = {}".format(
-                led_side_string, led_color_string))
-        else:
-            ev3.Leds.set_color(led_side, led_color)
-
 
 def main():
     print("--------------------------------------------")
@@ -102,14 +80,11 @@ def main():
     print("--------------------------------------------")
     ev3.Sound.speak("LED Button communication").wait()
 
-    # DONE: 3. Create an instance of your delegate class and an MQTT client, passing in the delegate object.
+    # TODO: 3. Create an instance of your delegate class and an MQTT client, passing in the delegate object.
     # Note: you can determine the variable names that you should use by looking at the errors underlined in later code.
     # Once you have that done connect the mqtt_client to the MQTT broker using the connect_to_pc method.
     # Note: on EV3 you call connect_to_pc, but in the PC code it will call connect_to_ev3
 
-    my_delegate = MyDelegate()
-    mqtt_client = com.MqttClient(my_delegate)
-    mqtt_client.connect_to_ev3()
 
 
     # Buttons on EV3 (these obviously assume TO DO: 3. is done)
@@ -145,7 +120,8 @@ def handle_button_press(button_state, mqtt_client, button_name):
 
         mqtt_client.send_message()
 
-# DONE: 5. Run this program on your EV3 and run m3_pc_led_button_communication.py on your PC at the same time.
+
+# TODO: 5. Run this program on your EV3 and run m3_pc_led_button_communication.py on your PC at the same time.
 # This will be the first time you've run a program on the robot today, but you'll remember how to do it (right?).
 # Hint: To be less confusing use two different computers for this task.  One to run the EV3 program (you) and one to run
 #   the PC program (a different person on your team).  It can be done from one computer but that gets confusing.
@@ -153,7 +129,7 @@ def handle_button_press(button_state, mqtt_client, button_name):
 # Press the buttons on the EV3 (up, down, left, right) and watch the Tkinter GUI on your PC.
 # When done, press the Back button on EV3 to end that program and click Quit on the Tkinter GUI.
 
-# DONE: 6. Call over a TA or instructor to sign your team's checkoff sheet (demo both LEDs and buttons).
+# TODO: 6. Call over a TA or instructor to sign your team's checkoff sheet (demo both LEDs and buttons).
 #
 # Observations you should make, when communicating using MQTT there is always a program running on EV3 and on your PC
 #   at the same time.  To make your life easier, you only write one of those programs this time (the on on the EV3).
